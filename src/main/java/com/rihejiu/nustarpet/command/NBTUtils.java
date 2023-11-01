3543e39e104dfa35ac994d36826ad429384d438d
NuStar <3318029085@qq.com>
2023-11-01 22:28:59 +0800

    增加删除NBT标签方法

--- src/main/java/com/rihejiu/nustarpet/command/NBTUtils.java
+++ src/main/java/com/rihejiu/nustarpet/command/NBTUtils.java
@@ -36,6 +36,15 @@ public class NBTUtils {
         tag.getString(key);
         return tag.getString(key);
     }
+    public static ItemStack removeTag(ItemStack itemStack, String key) {
+        net.minecraft.server.v1_12_R1.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = nmsItemStack.hasTag() ? nmsItemStack.getTag() : new NBTTagCompound();
+        if (tag.hasKey(key)) { // 检查标签是否存在
+            tag.remove(key); // 移除指定键的NBT标签
+        }
+        nmsItemStack.setTag(tag); // 将更新后的NBT标签设置回NMS ItemStack
+        return CraftItemStack.asBukkitCopy(nmsItemStack); // 转换回Bukkit ItemStack并返回
+    }
     public static int getTalentNum(String talent) {
         switch (talent){
             case "D" : return 1;
