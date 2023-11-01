de9821a6f30fcc5d163779f1836e84653428d05b
NuStar <3318029085@qq.com>
2023-11-01 22:28:05 +0800

    更新宠物UUID

---
+++ src/main/java/com/rihejiu/nustarpet/pet/PetUUID.java
@@ -0,0 +1,8 @@
+package com.rihejiu.nustarpet.pet;
+
+public class PetUUID {
+    public static String jyUUID = "7af4a886-a1c1-4044-ab45-c04ec89bbf45";
+    public static String zzUUID = "315935f1-f100-4a5e-bdf7-203e34ae7883";
+    public static String smUUID = "ef1653c4-5f31-4f25-91df-9afb055919cd";
+    public static String thUUID = "4ee789a6-2515-4017-82a8-466f0a3f9583";
+}
