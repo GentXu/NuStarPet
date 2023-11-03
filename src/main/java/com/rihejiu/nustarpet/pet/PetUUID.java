34881cafbae1eff589287dd9b5a21f76de0cd224
NuStar <3318029085@qq.com>
2023-11-03 21:13:41 +0800

    恐狼UUID

--- src/main/java/com/rihejiu/nustarpet/pet/PetUUID.java
+++ src/main/java/com/rihejiu/nustarpet/pet/PetUUID.java
@@ -5,4 +5,5 @@ public class PetUUID {
     public static String zzUUID = "315935f1-f100-4a5e-bdf7-203e34ae7883";
     public static String smUUID = "ef1653c4-5f31-4f25-91df-9afb055919cd";
     public static String thUUID = "4ee789a6-2515-4017-82a8-466f0a3f9583";
+    public static String klUUID = "eaa388e5-2ed8-4c13-8276-09988fb2ab33";
 }
